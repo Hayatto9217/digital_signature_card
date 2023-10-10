006cb4c65b22dd393d1e63fe7d98ccb5f4043101
Hayatto9217 <harutotokuyama869217@gmail.com>
2023-10-10 13:15:17 +0900

    技術サイトへのリンクを追加!!

--- build.js
+++ build.js
@@ -15,23 +15,23 @@ const options = {
 
 // Text + chalk definitions
 const data = {
-  name: chalk.white('           Tierney Cyren'),
-  handle: chalk.white('bitandbang'),
-  shorthandle: chalk.white('bnb'),
-  work: chalk.white('Principal Developer Advocate at Twilio (::)'),
-  twitter: chalk.gray('https://twitter.com/') + chalk.cyan('bitandbang'),
-  mastodon: chalk.gray('https://mastodon.social/') + chalk.magenta('@bnb'),
+  name: chalk.white('           Haruto Tokuyama'),
+  handle: chalk.white('Haruto8692'),
+  shorthandle: chalk.white('Haruto'),
+  work: chalk.white('Software engineers interested in quantum information(::)'),
+  twitter: chalk.gray('https://twitter.com/haruto869217') + chalk.cyan('haruto869217'),
+  qiita: chalk.yellow('https://qiita.com/haruto19') + chalk.cyan('haruto19'),
+  zenn: chalk.blue('https://zenn.dev/haruto869217') + chalk.cyan('haruto869217'),
+  mastodon: chalk.gray('https://mastodon.social/') + chalk.magenta('@haruto869217'),
   npm: chalk.gray('https://npmjs.com/') + chalk.red('~bnb'),
-  github: chalk.gray('https://github.com/') + chalk.green('bnb'),
-  linkedin: chalk.gray('https://linkedin.com/in/') + chalk.blue('bitandbang'),
-  web: chalk.cyan('https://bnb.im'),
-  npx: chalk.red('npx') + ' ' + chalk.white('bitandbang'),
+  github: chalk.gray('https://github.com/') + chalk.green('Hayatto9217'),
+  web: chalk.cyan('https://haru.haru869217.com/') + chalk.green('https://haru.haru869217.com/'),
+  npx: chalk.red('npx') + ' ' + chalk.white('haruto8692'),
   labelWork: chalk.white.bold('    Work:'),
-  labelTwitter: chalk.white.bold(' Twitter:'),
+  labelTwitter: chalk.white.bold(' X:'),
   labelMastodon: chalk.white.bold('Mastodon:'),
   labelnpm: chalk.white.bold('     npm:'),
   labelGitHub: chalk.white.bold('  GitHub:'),
-  labelLinkedIn: chalk.white.bold('LinkedIn:'),
   labelWeb: chalk.white.bold('     Web:'),
   labelCard: chalk.white.bold('    Card:')
 }
@@ -40,11 +40,13 @@ const data = {
 const newline = '\n'
 const heading = `${data.name} / ${data.handle} / ${data.shorthandle}`
 const working = `${data.labelWork}  ${data.work}`
-const twittering = `${data.labelTwitter}  ${data.twitter}`
+const twittering = `${data.labelX}  ${data.x}`
+const qiitaing = `${data.labelQiita}  ${data.qiita}`
+const zenning = `${data.labelZenning} ${data.zenn}`
 const mastodoning = `${data.labelMastodon}  ${data.mastodon}`
 const npming = `${data.labelnpm}  ${data.npm}`
 const githubing = `${data.labelGitHub}  ${data.github}`
-const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`
+//const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`
 const webing = `${data.labelWeb}  ${data.web}`
 const carding = `${data.labelCard}  ${data.npx}`
 
@@ -52,12 +54,14 @@ const carding = `${data.labelCard}  ${data.npx}`
 const output = heading + // data.name + data.handle
                newline + newline + // Add one whole blank line
                working + newline + // data.labelWork + data.work
-               twittering + newline + // data.labelTwitter + data.twitter
+               xing + newline + // data.labelTwitter + data.twitter
+               qiitaing + newline + //data.labelqiita + data.qiita
+               zenning + newline + //data.labelzenn + data.zenn
                mastodoning + newline + // data.labelTwitter + data.twitter
                npming + newline + // data.labelnpm + data.npm
                githubing + newline + // data.labelGitHub + data.github
-               linkedining + newline + // data.labelLinkedIn + data.linkedin
+               //linkedining + newline + // data.labelLinkedIn + data.linkedin
                webing + newline + newline + // data.labelWeb + data.web
                carding // data.labelCard + data.npx
 
-fs.writeFileSync(path.join(__dirname, 'bin/output'), chalk.green(boxen(output, options)))
+fs.writeFileSync(path.join(__dirname, 'bin/output'), chalk.blue(boxen(output, options)))
