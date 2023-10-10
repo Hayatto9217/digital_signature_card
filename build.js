83f4e7d71f761a9f4139e0b7846da82a33aa25fd
Hayatto9217 <harutotokuyama869217@gmail.com>
2023-10-10 17:31:28 +0900

    constからimport でESM形式に対応

--- build.js
+++ build.js
@@ -1,10 +1,10 @@
 'use strict'
 
 // Pull in our modules
-const chalk = require('chalk')
-const boxen = require('boxen')
-const fs = require('fs')
-const path = require('path')
+import chalk from 'chalk';
+import boxen from 'boxen';
+import fs from 'fs';
+import path from 'path';
 
 // Define options for Boxen
 const options = {
